c37a9730dd633b51fb2dfb35801de7eb927fc75c
juan- <juan-@LAPTOP-GTL45HNS>
2024-02-17 09:16:03 -0600

    Add project files.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContext = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> ListadoEquipo = (from e in _equiposContext.Equipos select e).ToList();
+
+            if (ListadoEquipo.Count == 0)
+                return NotFound();
+            return Ok(ListadoEquipo);
+        }
+    }
+}
